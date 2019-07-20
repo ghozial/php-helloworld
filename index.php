134fdfe090d3027e6fdf8b9b6240e9f67d7b5f91
Ahmad Alghozi Ramadhan <37783343+ghozial@users.noreply.github.com>
2019-07-21 03:34:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?PHP
 $data = '{
-"nama":"lele",
-"alamat":"tangerang"
-"nohape";"082124850922"
+"nama":"dini,daka,pipih",
+"alamat":"tangerang,jakarta"
+"nohape";"082124850922,081214845043"
+"username";"dini001,daka002,pipih003"
+"password";"diku001,daka002,papah003"
 }';
 header('Content-Type: application/json');
 echo $data;
